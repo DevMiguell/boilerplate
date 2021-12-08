a4b4aca42b8c88b62294eb99b875168e5189557c
DevMiguell <buguel2000@gmail.com>
2021-12-08 12:58:28 -0300

    chore: add storybook and configure

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  // moduleNameMapper: {
+  //   '^styled-components':
+  //     '<roorDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  // }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,9 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="shortcut icon" href="img/icon-512.png" type="image/png" />
+        <link rel="apple-touch-icon" href="img/icon-512.png" type="image/png" />
         <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
